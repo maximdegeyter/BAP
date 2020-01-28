fb29e8cae92a9aa330c76c3070a8ceac2a97946d
maximdegeyter <maximdegeyter@outlook.com>
2020-01-28 10:02:58 +0100

    created startfile

---
+++ project/src/index.js
@@ -0,0 +1,13 @@
+import './style.css';
+import 'phaser';
+import Game from './js/classes/Game.js';
+
+
+{
+  const init = () => {
+    console.log(`GAME OPSTARTEN`);
+    new Game();
+  };
+  init();
+}
+
---
+++ project/src/js/classes/Game.js
@@ -0,0 +1,7 @@
+class Game extends Phaser.Game{
+  constructor(){
+    super();
+    console.log(`Constructor Game class`);
+  }
+}
+export default Game;
